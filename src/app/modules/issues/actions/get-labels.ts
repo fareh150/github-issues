354a6cc4404e7eae06c1f9a1bbb11a5c93bc56e5
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-09 18:21:38 +0100

    usage sleep

--- src/app/modules/issues/actions/get-labels.ts
+++ src/app/modules/issues/actions/get-labels.ts
@@ -1,7 +1,9 @@
+import { sleep } from "@helpers/sleep";
 import { GitHubLabel } from "../interfaces/github-label.interface";
 
 export const getLabels = async (): Promise<GitHubLabel[]> =>
 {
+  await sleep(1500);
   try {
     const resp = await fetch(`https://api.github.com/repos/angular/angular/labels`);
 

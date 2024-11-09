a3c9720fbde1c5d38dfff540b7f6454298ff88e7
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-09 17:45:14 +0100

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
     loadComponent:() => import('./modules/issues/pages/issues-list/issues-list-page.component')
   },
   {
-    path: 'issues/:number',
+    path: 'issue/:number',
     loadComponent: () => import('./modules/issues/pages/issue/issue-page.component')
   },
   {

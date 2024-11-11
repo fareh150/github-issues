d6372f24c0fe1d75395c33fa705accb2981f9a12
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-11 08:47:51 +0100

    Add getIssueAction

---
+++ src/app/modules/issues/actions/get-issues.action.ts
@@ -0,0 +1,34 @@
+import { sleep } from "@helpers/sleep";
+import { GitHubLabel } from "../interfaces/github-label.interface";
+import { environment } from "src/environments/environment.development";
+import { GithubIssue } from "../interfaces";
+
+const BASE_URL = environment.baseUrl;
+const GITHUB_TOKEN = environment.gitHubToken;
+
+export const getIssues = async (): Promise<GithubIssue[]> =>
+{
+  await sleep(1500);
+  try {
+    const resp = await fetch(
+      `${BASE_URL}/issues`,
+      {
+        headers: {
+          Authorization: `Bearer ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    if (!resp.ok)
+    {
+      throw "Can't load issues";
+    }
+
+    const issues: GithubIssue[] = await resp.json();
+    console.log({ issues });
+
+    return issues;
+  } catch (error) {
+    throw "Can't load issues";
+  }
+}

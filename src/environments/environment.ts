a47c631019bd5bd2aacc2fa43c6eefe578a50f75
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-11 08:48:19 +0100

    add enviroments

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment =
+{
+  baseUrl: `https://api.github.com/repos/angular/angular`,
+  gitHubToken: `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`
+};

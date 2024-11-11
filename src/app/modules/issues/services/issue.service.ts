916809a1ef1b277b69129ade373545c135564837
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-11 17:42:50 +0100

    [EXAM]  Create query in service

--- src/app/modules/issues/services/issue.service.ts
+++ src/app/modules/issues/services/issue.service.ts
@@ -1,18 +1,28 @@
 import { Injectable, signal } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { getIssueByNumber } from '../actions';
+import { getIssueCommentsByNumber } from '../actions/get-issue-comments-by-number.action';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssueService {
   private issueNumber = signal<string|null>(null)
+
   issueQuery = injectQuery(() =>(
     {
       queryKey: ['issue', this.issueNumber()],
       queryFn: () => getIssueByNumber(this.issueNumber()!),
       enable: this.issueNumber() !== null
     }));
+
+  issueCommentsQuery = injectQuery(() => (
+    {
+      queryKey: ['issue', this.issueNumber(), 'comments'],
+      queryFn: () => getIssueCommentsByNumber(this.issueNumber()!),
+      enable: this.issueNumber() !== null
+    }
+    ))
 
   setIssueNumber = (issueId: string) =>
   {

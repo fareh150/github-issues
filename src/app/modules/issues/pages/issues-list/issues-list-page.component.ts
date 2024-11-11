0f6f47c53fd982f9659536d0715e794c1a06b526
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-11 10:06:15 +0100

    fix issue-list-page

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { IssuesService } from '../../services/issues.service';
 import { LabelSelectorComponent } from "../../components/label-selector/label-selector.component";
+import { IssueItemComponent } from '../../components/issue-item/issue-item.component';
 
 @Component({
   selector: 'issues-list-page',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
-    LabelSelectorComponent
+    LabelSelectorComponent,
+    IssueItemComponent
 ],
   templateUrl: './issues-list-page.component.html',
 })
@@ -20,5 +20,9 @@ export default class IssuesListPageComponent {
   get labelsQuery()
   {
     return this.issuesService.labelsQuery;
+  }
+  get issuesQuery()
+  {
+    return this.issuesService.issuesQuery;
   }
 }

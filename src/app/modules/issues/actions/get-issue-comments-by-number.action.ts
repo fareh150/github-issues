cb76209f1b4c3c8327e2f9ad7cbe5f39aa6a5639
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-11 17:42:32 +0100

    [EXAM] create action to get data

---
+++ src/app/modules/issues/actions/get-issue-comments-by-number.action.ts
@@ -0,0 +1,33 @@
+import { sleep } from "@helpers/sleep";
+import { environment } from "src/environments/environment.development";
+import { GithubIssue } from "../interfaces";
+
+const BASE_URL = environment.baseUrl;
+const GITHUB_TOKEN = environment.gitHubToken;
+
+export const getIssueCommentsByNumber = async (issueNumber:string): Promise<GithubIssue[]> =>
+{
+  await sleep(1500);
+  try {
+    const resp = await fetch(
+      `${BASE_URL}/issues/${issueNumber}/comments`,
+      {
+        headers: {
+          Authorization: `Bearer ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    if (!resp.ok)
+    {
+      throw "Can't load issue comments";
+    }
+
+    const issueComments: GithubIssue[] = await resp.json();
+    console.log({issueComments});
+
+    return issueComments;
+  } catch (error) {
+    throw "Can't load issues";
+  }
+}

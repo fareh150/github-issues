cb2a31e6c796cb0b963addbae7ad33e0f8ccc444
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-11 08:48:02 +0100

    fix export

--- src/app/modules/issues/services/issues.service.ts
+++ src/app/modules/issues/services/issues.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
-import { getLabels } from '../actions/get-labels';
+import { getLabels } from '../actions/get-labels.action';
 
 @Injectable({
   providedIn: 'root'

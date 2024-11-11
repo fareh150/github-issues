b187526af88050a9853b4433d1be96a6e23db620
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-11 10:11:52 +0100

    Get IssueNumber from ROUTE <<<< important

--- src/app/modules/issues/pages/issue/issue-page.component.ts
+++ src/app/modules/issues/pages/issue/issue-page.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-issue-page',
@@ -9,4 +12,14 @@ import { Component } from '@angular/core';
   ],
   templateUrl: './issue-page.component.html',
 })
-export default class IssuePageComponent { }
+export default class IssuePageComponent {
+  route = inject(ActivatedRoute);
+  issueNumber = toSignal(
+    this.route.paramMap
+    .pipe(
+      map( params => params.get('number') ?? '' ),
+      tap(console.log)
+
+    )
+  )
+}

46789e43fa00d9bc79d43e42432c9d929e82dc4a
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-18 15:06:40 +0100

    prepare service to detect the status

--- src/app/modules/issues/services/issues.service.ts
+++ src/app/modules/issues/services/issues.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { getIssues, getLabels } from '../actions';
+import { State } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
+  selectedState = signal<State>(State.All);
+
   labelsQuery = injectQuery(() =>(
   {
     queryKey: ['labels'],
@@ -14,7 +17,11 @@ export class IssuesService {
 
   issuesQuery = injectQuery(() =>(
   {
-    queryKey: ['issues'],
-    queryFn: () => getIssues(),
+    queryKey: ['issues', this.selectedState()],
+    queryFn: () => getIssues(this.selectedState()),
   }));
+
+  showIssuesByState(state: State) {
+    this.selectedState.set(state);
+  }
 }

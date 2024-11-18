897816fb556042419f5670bb470c5f48ea05bd4c
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-18 15:05:22 +0100

    [FETCH] Prepare action to filters
    -agregamos el parametro
    -le ponemos un valor por defecto , si es que no envian nada
    -creamos el valor de los parametros
    -con append lo agregamos
    -agregamos los parametros en la url

--- src/app/modules/issues/actions/get-issues.action.ts
+++ src/app/modules/issues/actions/get-issues.action.ts
@@ -1,16 +1,23 @@
 import { sleep } from "@helpers/sleep";
 import { environment } from "src/environments/environment.development";
-import { GithubIssue } from "../interfaces";
+import { GithubIssue, State } from "../interfaces";
 
 const BASE_URL = environment.baseUrl;
 const GITHUB_TOKEN = environment.gitHubToken;
 
-export const getIssues = async (): Promise<GithubIssue[]> =>
+export const getIssues = async (
+  state: State = State.All,
+): Promise<GithubIssue[]> =>
 {
   await sleep(1500);
+
+  // ! uso de parametros de URL para peticiones fetch
+  const params = new URLSearchParams();
+  params.append("state", state);
+
   try {
     const resp = await fetch(
-      `${BASE_URL}/issues`,
+      `${BASE_URL}/issues?${params}`,
       {
         headers: {
           Authorization: `Bearer ${GITHUB_TOKEN}`,
